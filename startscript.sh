770cd0e6b97297bb3c69cba1fd6930150e138916
Dominik Stucki <dstucki@puzzle.ch>
2019-11-13 09:31:36 +0100

    make sieve migration more bulletproof and cleanup the script

--- startscript.sh
+++ startscript.sh
@@ -18,11 +18,11 @@ sieve=old_sieve/var/lib/imap/sieve/domain/?/$domain/?         # Dovecot sieve.
 log=/tmp/mail-migration-conversion.log                        # Log of successful conversions.
 err=/tmp/mail-migration-error.log                             # Log of conversion errors.
 
-mkdir -p $out/$domain                                         # Create domain folder
+# Create domain folder
+mkdir -p $out/$domain
 
 for u in `find $in/. \! -name . -prune -exec basename \{\} \;`
 do
-
     ./cyrus2dovecot --cyrus-inbox $in/$u                             \
                     --dovecot-inbox $out/$domain/$u@$domain/maildir  \
                     --cyrus-sieve $sieve/$u/roundcube.script         \
@@ -30,9 +30,12 @@ do
                     --edit-foldernames 's/\./\//g'                   \
                     $u 2>&1 >>$log | tee -a $err >&2
 
-    mv $out/$domain/$u@$domain/maildir/sieve $out/$domain/$u@$domain/                               # Fix sieve path issue
-    ln -s $out/$domain/$u@$domain/sieve/managesieve.sieve  $out/$domain/$u@$domain/.dovecot.sieve   # ""                ""
+    # Fix sieve path issue
+    cp -rf $out/$domain/$u@$domain/maildir/sieve $out/$domain/$u@$domain/
+    rm -rf $out/$domain/$u@$domain/maildir/sieve
+    ln -sf $out/$domain/$u@$domain/sieve/managesieve.sieve  $out/$domain/$u@$domain/.dovecot.sieve
 done
 
-chown -R vmail:vmail $out/$domain                               # Fix owner
+# Fix owner
+chown -R vmail:vmail $out/$domain
 chmod -R 700 $out/$domain
